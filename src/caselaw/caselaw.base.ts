33dab9757fa95b28f61c6547bc2cf38a31ac7797
aalex <aalex@LAPTOP>
2025-11-04 19:52:43 +0100

    added validation decorators

--- src/caselaw/caselaw.base.ts
+++ src/caselaw/caselaw.base.ts
@@ -1,26 +1,34 @@
 import { Field, ObjectType, InputType } from '@nestjs/graphql';
+import { IsString, IsDateString } from 'class-validator';
 
-@ObjectType()
 @InputType('CaseLawBaseInput')
+@ObjectType()
 export class CaseLawBase {
     @Field()
+    @IsString()
     title: string;
 
     @Field()
+    @IsString()
     decisionType: string;
 
     @Field(() => Date)
+    @IsDateString()
     dateOfDecision: Date;
 
     @Field()
+    @IsString()
     office: string;
 
     @Field()
+    @IsString()
     court: string;
 
     @Field()
+    @IsString()
     caseNumber: string;
 
     @Field()
+    @IsString()
     summary: string;
 }
